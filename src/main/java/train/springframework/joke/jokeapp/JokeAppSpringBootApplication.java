23626551b0c6a3bfc2a1f743a6db6159a4049119
SJRB\skapur1 <sanjay.kapur@sjrb.ca>
2018-11-26 13:59:30 -0700

    Init Commit

---
+++ src/main/java/train/springframework/joke/jokeapp/JokeAppSpringBootApplication.java
@@ -0,0 +1,12 @@
+package train.springframework.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeAppSpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/train/springframework/joke/jokeapp/JokeAppSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package train.springframework.joke.jokeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JokeAppSpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
